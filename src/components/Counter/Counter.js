7938f795e18ca091a53aa8e7e94bec91e8e1da05
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-13 16:10:38 +0530

    counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,9 +3,15 @@ import './Counter.css'
 
 class Counter extends Component {
     state = {count:0}
+    
     onIncrement = () => {
         this.setState(previousState => ({
             count:previousState.count+1
+        }))
+    }
+    onDecrement = () => {
+        this.setState(previousState => ({
+            count:previousState.count-1
         }))
     }
     render() {
@@ -13,10 +19,11 @@ class Counter extends Component {
         return (
             <div className="Count-Page">
                 <div>
-                    <h1>The Button has been clicked <span className="count">{count}</span> times.</h1>
+                    <h1 className="heading">The Button has been clicked <span className="count">{count}</span> times.</h1>
                     <p className="para">Click the button to increase the count!</p>
                     <div className="btn-container">
-                        <button className="btn" onClick={this.onIncrement}>Click</button>
+                        <button className="btn-1" onClick={this.onIncrement}>Increase</button>
+                        <button className="btn-2" onClick={this.onDecrement}>Decrease</button>
                     </div>
                 </div>
             </div>
